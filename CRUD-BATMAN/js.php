e3e63379c735b982f9ee4348f7a584d50f7f91c0
Vinciboi30 <paulvincentadarlo30@gmail.com>
2026-06-02 11:31:04 +0800

    Delete CRUD-BATMAN directory

--- CRUD-BATMAN/js.php
+++
@@ -1,2 +0,0 @@
-<script src="https://kit.fontawesome.com/62af407608.js" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
